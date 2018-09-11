574ed449083a13262f0c15f6732a77244deb00e6
Sebastian Palm <sebastian.palm91@gmail.com>
2018-09-11 23:06:01 +0200

    some structure

---
+++ client/src/Components/Footer/footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import {FooterContainer} from './styles';
+
+class Footer extends Component {
+    render () {
+        return (
+            <FooterContainer>
+            </FooterContainer>
+        )
+    }
+}
+
+export default Footer;
---
+++ client/src/Components/Footer/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const FooterContainer = styled.div `
+    background-color: red;
+    height: 100px;
+    width: 100%;
+    position: absolute;
+    bottom: 0;
+`
