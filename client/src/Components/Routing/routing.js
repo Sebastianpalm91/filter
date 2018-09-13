cedcfaf9e90877299e3b8e805565fcd26f4979f6
Sebastian Palm <sebastian.palm91@gmail.com>
2018-09-13 15:20:19 +0200

    added routing and transition group

---
+++ client/src/Components/Routing/routing.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import { Switch, Route, withRouter } from 'react-router-dom';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import './styles.css';
+
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import ENTER_COMPONENT_NAME from 'ENTER_COMPONENT_NAME';
+
+class Routing extends Component {
+    render () {
+        return (
+                <TransitionGroup className="transition-group">
+                    <CSSTransition
+                        key={this.props.location.key}
+                        timeout={{ enter: 1000, exit: 1000 }}
+                        classNames="fadeIn"
+                    >
+                        <section className="route-section">
+                            <Switch location={this.props.location}>
+                                <Route exact path='/' /*component={ENTER_COMPONENT_NAME}*/ render={() => <ENTER_COMPONENT_NAME  />}/>
+                                <Route exact path='/' render={() => <ENTER_COMPONENT_NAME  />}/>
+                                <Route exact path='/' component={ENTER_COMPONENT_NAME}/>
+                                <Route exact path='/' component={ENTER_COMPONENT_NAME}/>
+                            </Switch>
+                        </section>
+                    </CSSTransition>
+                </TransitionGroup>
+        )
+    }
+}
+
+export default withRouter(Routing);
