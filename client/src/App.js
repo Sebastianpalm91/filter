562bef0bee1c2c0aa553209e0a58ad157695e73b
Sebastian Palm <sebastian.palm91@gmail.com>
2018-09-18 15:04:04 +0200

    added hamburger menu

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import {Container} from './Components/GeneralStyles/styles';
 import Footer from './Components/Footer/footer';
-import Hamburger from './Components/Hamburger/footer';
+import Hamburger from './Components/Menu/navigation';
 import './App.css'
 
 class App extends Component {
   render() {
     return (
         <div className="Container">
+            <Hamburger></Hamburger>
             <Footer></Footer>
         </div>
     );
