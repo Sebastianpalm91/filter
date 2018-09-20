2e6984294a116f12f69e940bc0ca93fc6c860821
Sebastian Palm <sebastian.palm91@gmail.com>
2018-09-20 15:52:10 +0200

    scrollbar

--- client/src/Components/Pages/article/readingcomponents/readingview/readingview.js
+++ client/src/Components/Pages/article/readingcomponents/readingview/readingview.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
 import { Container, Wrapper, Title, Paragraph, Tag, Published } from '../../article/styles';
-
+import ScrollBar from '../scrollbar/';
 
 class ReadingView extends Component {
 
     render() {
         return (
             <Container>
+                <ScrollBar>
                 <Wrapper isReadingView={true}>
                     <Title isReadingView={true}>{this.props.title}</Title>
                     <Tag isReadingView={true}>{this.props.tag}<Published isReadingView={true}>{this.props.published_at}</Published></Tag>
                     <Paragraph isReadingView={true}>{this.props.article}</Paragraph>
                 </Wrapper>
+                </ScrollBar>
             </Container>
         );
     }
--- client/src/Components/Pages/article/readingcomponents/scrollbar/index.js
+++ client/src/Components/Pages/article/readingcomponents/scrollbar/index.js
@@ -1,16 +1,14 @@
 import React, { Component } from 'react';
-import { Container, Wrapper, Title, Paragraph, Tag, Published } from '../../article/styles';
-
+import { Container, Wrapper, Scrollbar, ScrollbarTrack } from './styles';
+import './styles.css';
 
 class ScrollBar extends Component {
 
     render() {
         return (
             <Container>
-                <Wrapper isReadingView={true}>
-                    <Title isReadingView={true}>{this.props.title}</Title>
-                    <Tag isReadingView={true}>{this.props.tag}<Published isReadingView={true}>{this.props.published_at}</Published></Tag>
-                    <Paragraph isReadingView={true}>{this.props.article}</Paragraph>
+                <Wrapper>
+                    
                 </Wrapper>
             </Container>
         );
