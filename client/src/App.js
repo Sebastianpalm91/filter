093f25fbaa12a2e4e5e79bd8c0a862ac2a04461c
Sebastian Palm <sebastian.palm91@gmail.com>
2018-09-20 13:40:39 +0200

    nothing to see here

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
 // import {Container} from './Components/GeneralStyles/styles';
 import Footer from './Components/Footer/footer';
-import Hamburger from './Components/Menu/navigation';
+// import Hamburger from './Components/Menu/navigation';
 import './App.css'
 
 import Routing from './Components/Routing/routing';
 
 
+// <Hamburger />
+// <Footer></Footer>
 class App extends Component {
   render() {
     return (
         <div className="Container">
-            <Hamburger></Hamburger>
             <Routing />
-            <Footer></Footer>
         </div>
     );
   }
