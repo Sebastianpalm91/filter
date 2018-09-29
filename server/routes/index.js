9a1f0817465dd7f4cf71d0afe81d7f61754aa8c0
AddeNordh <nordh.andreas@hotmail.com>
2018-09-29 21:05:40 +0200

    images now goes through imgOptim api before uploaded

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,9 +5,11 @@ const path = require('path');
 const fs = require('fs-extra');
 const moment = require('moment');
 const busboy = require('connect-busboy');
-const formidable = require('formidable');
+const superagent = require('superagent');
 
 const ArticleModel = mongoose.model('Article');
+
+const IMG_OPTIM_USERNAME = 'jrvdhpbpnd';
 // const UserModel = mongoose.model('User');
 //
 moment.locale('sv');
@@ -30,7 +32,7 @@ router.get('/articles/:title', async (req, res) => {
 
 router.post('/article/upload', async (req, res) => {
 
-    const tmpData = {
+    const tmpDataHolder = {
         thumbnail: '',
         title: '',
         article: '',
@@ -40,14 +42,20 @@ router.post('/article/upload', async (req, res) => {
         author: '',
 
     }
+
     try {
 
         req.pipe(req.busboy);
         req.busboy.on('file', (fieldname, file, filename) => {
+
             const path = `${__dirname}/../public/imgs`;
             filename = `${Date.now()}-${filename}`;
-            const fstream = fs.createWriteStream(`${path}/${filename}`);
-            tmpData[fieldname] = filename;
+            const pathToImg = `${path}/${filename}`;
+
+            const fstream = fs.createWriteStream(pathToImg);
+
+            tmpDataHolder[fieldname] = filename;
+
             fstream.on('error', err => {
                 console.log(`fstream error: ${err}`);
                 fstream.unpipe();
@@ -56,16 +64,31 @@ router.post('/article/upload', async (req, res) => {
             });
 
             file.pipe(fstream);
+
+            fstream.on('finish', () => {
+                superagent.post(`https://im2.io/${IMG_OPTIM_USERNAME}/full,quality=low`)
+                    .attach('file', `public/imgs/${filename}`)
+                    .then(res => {
+                        const imgData = res.body;
+
+                        fs.writeFile(`public/imgs/${filename}`, imgData);
+                    })
+                    .catch(err => {
+                        console.log(err);
+                    });
+                });
+
+
             res.status('200');
         });
 
         req.busboy.on('field', (key, value, keyTruncated, valueTruncated) => {
-            tmpData[key] = value;
+            tmpDataHolder[key] = value;
         });
 
         req.busboy.on('finish', async () => {
 
-            const { thumbnail, title, article: content, tag, published_at, author_img, author } = tmpData;
+            const { thumbnail, title, article: content, tag, published_at, author_img, author } = tmpDataHolder;
 
             const articleData = {
                 title,
@@ -84,7 +107,7 @@ router.post('/article/upload', async (req, res) => {
             await article.save();
 
             res.status('200');
-        })
+        });
 
 
     } catch (err) {
