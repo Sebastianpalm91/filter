4358493daafb12e3d0dc6bac3afb16caae7754ff
AddeNordh <nordh.andreas@hotmail.com>
2018-10-10 18:59:33 +0200

    updated to get the right data

--- client/src/Components/Pages/article/readingcomponents/readingview/readingview.js
+++ client/src/Components/Pages/article/readingcomponents/readingview/readingview.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Container, Wrapper, Title, Paragraph, LoadingArticle, ArticleInfoWrapper, InfoName, InfoWork, MagazineNumber, Highlighted, Preamble, Thumbnail, } from './styles';
+import { Container, Wrapper, Title, Paragraph, LoadingArticle, ArticleInfoWrapper, InfoName, InfoWork, MagazineNumber, Highlighted, Preamble, Thumbnail,ReadingTime } from './styles';
 import ScrollBar from '../scrollbar/';
 import { FaHeadphones, } from 'react-icons/fa';
 import './removeScroll.css';
@@ -9,23 +9,19 @@ class ReadingView extends Component {
         isHide: false,
     }
 
-    componentDidMount() {
-        window.addEventListener('scroll', this.hasScrolled)
-    }
-
     getEstimatedReadingTime(article) {
         const words = article.split(' ').length;
         const minutes = Math.round(words / 200);
 
-        return minutes;
+        return `${minutes} min`;
     }
 
     render() {
 
         if (this.props.dataHasloaded) {
-            const { article, article: { name }, illustrations, thumbnail, magazine: { number }, preamble, published_at, tag, title } = this.props;
+            const { article, author: { name }, illustrations, thumbnail, magazine: { number }, preamble, published_at, tag, title } = this.props;
             return (
-                <Container>
+                <Container data-name='article'>
                     <Thumbnail src={`http://localhost:1337/public/imgs/${thumbnail}`}></Thumbnail>
                     <ScrollBar />
                     <Wrapper>
@@ -35,15 +31,15 @@ class ReadingView extends Component {
                             <InfoWork paddingLeft={true}>illustration: </InfoWork><InfoName>{illustrations}</InfoName>
                             <MagazineNumber>
                                 <Highlighted>{tag}</Highlighted> publicerad i filter #{number} ( {published_at} )
-                                <br></br>
+                                    <br />
+                                    <ReadingTime>ESTIMERAT LÄSTID: {this.getEstimatedReadingTime(article)}</ReadingTime>
                                 <Highlighted style={{ display: 'flex', alignItems: 'center', marginTop: '5px', cursor: 'pointer' }}><FaHeadphones style={{ fill: '#E1AB99', height: '20px', width: '20px', marginRight: '5px'}} />LYSSNA</Highlighted>
                             </MagazineNumber>
                         </ArticleInfoWrapper>
                         <Preamble>
                             <Paragraph>{preamble}</Paragraph>
                         </Preamble>
-                        <Paragraph dangerouslySetInnerHTML={{ __html: article }}>
-                        </Paragraph>
+                        <Paragraph dangerouslySetInnerHTML={{ __html: article }}></Paragraph>
                     </Wrapper>
                 </Container>
             );
